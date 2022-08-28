c96b58f93159ed11e8e3110eae1feea8045b3b87
Gavyn Holt <gavynholt@gmail.com>
2022-08-28 11:03:45 -0400

    chore: change root folder name

--- projects/ngx-pipe-utils/src/public-api.ts
+++ projects/ngx-pipe-utils/src/public-api.ts
@@ -1,7 +1,6 @@
 /*
  * Public API Surface of pipe-utils
  */
-
 export * from './lib/pipe-utils.service';
 export * from './lib/milliseconds-to-duration.pipe';
 export * from './lib/pipe-utils.module';
